b18b8fa4b57a136d39b2eca3550d23cb65ad6f98
Fahad Hossain <fahadh.dev@gmail.com>
2022-03-09 12:15:24 +0600

    String.prototype.charAt #7

--- src/string/charAt/customCharAt.spec.ts
+++ src/string/charAt/customCharAt.spec.ts
@@ -3,14 +3,14 @@ import './customCharAt';
 describe('String.prototype.customCharAt', () => {
   const input = 'Hello';
   test(`From '${input}' with index 1, should return 'e'`, () => {
-    expect(input.customCharAt(1)).toBe('e');
+    expect(input.customCharAt(1)).toBe(input.charAt(1));
   });
 
   test(`From '${input}' with index -1, should return ''`, () => {
-    expect(input.customCharAt(-1)).toBe('');
+    expect(input.customCharAt(-1)).toBe(input.charAt(-1));
   });
 
   test(`From '${input}' with index 20, should return ''`, () => {
-    expect(input.customCharAt(20)).toBe('');
+    expect(input.customCharAt(20)).toBe(input.charAt(20));
   });
 });
